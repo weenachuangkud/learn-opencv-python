e368f7b4bd2c9e0920a680a0a5cfc0bd5ce8fe84
Mawin CK <weena.chuangkud@gmail.com>
2026-03-17 05:11:12 +0700

    Update

--- src/read_cat.py
+++ src/read_cat.py
@@ -6,4 +6,7 @@ cv.imshow('Cat', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray cat', gray)
 
+blur = cv.GaussianBlur(img, (5,5), cv.BORDER_DEFAULT)
+cv.imshow('Blur cat', blur)
+
 cv.waitKey(0)
