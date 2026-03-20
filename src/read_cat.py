f551ab5a81c355203c74386a72964b1b1f043a18
Mawin CK <weena.chuangkud@gmail.com>
2026-03-21 03:12:49 +0700

    Update

--- src/read_cat.py
+++ src/read_cat.py
@@ -18,7 +18,7 @@ cv.imshow('Dilated', dilated)
 resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
 cv.imshow('Resized', resized)
 
-cropped = img[50:200, 200:400]
+cropped = img[50:300, 200:400]
 cv.imshow('Cropped', cropped)
 
 cv.waitKey(0)
